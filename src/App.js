2c39ffc37ae947cb11a17a0e7a995c38cc99f33e
Jun <cjmsakakibara@gmail.com>
2021-05-21 18:48:03 -0300

    working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,225 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import dayjs from "dayjs";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import React, { useState } from "react";
+import {
+  getDailyWeather,
+  getGeocodingWeatherData,
+  getHourlyWeatherByDay,
+  getWeeklyWeatherByDay,
+} from "./api";
+
+const changeDayFromToday = (referenceDay) => {
+  const today = dayjs();
+  if (referenceDay > 0) {
+    return today.add(referenceDay, "day");
+  } else {
+    referenceDay = -referenceDay;
+    return today.subtract(referenceDay, "day");
+  }
+};
+
+const celsiusToFahrenheit = (celsius) => {
+  if (celsius === "indisponível") {
+    return "indisponível";
+  }
+
+  return parseFloat(((celsius * 9) / 5 + 32).toFixed(2));
+};
+
+const Calendar = ({
+  startDate,
+  setStartDate,
+  tempViewMode,
+  handleGetWeatherData,
+}) => {
+  return (
+    <div>
+      Selecione o dia:
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        minDate={changeDayFromToday(-5).$d}
+        maxDate={
+          tempViewMode === "Hora"
+            ? changeDayFromToday(1).$d
+            : changeDayFromToday(7).$d
+        }
+        showDisabledMonthNavigation
+        inline
+      />
+      <button
+        onClick={() => {
+          handleGetWeatherData(startDate);
+        }}
+      >
+        Escolher data
+      </button>
+    </div>
+  );
+};
+
+const Filter = ({
+  locale,
+  setLocale,
+  tempViewMode,
+  setTempViewMode,
+  startDate,
+}) => {
+  return (
+    <form>
+      <label>Localidade </label>
+      <input
+        type="text"
+        value={locale}
+        onChange={(e) => setLocale(e.target.value)}
+      />
+      <label>Exibir por: </label>
+      <select
+        value={tempViewMode}
+        onChange={(e) => setTempViewMode(e.target.value)}
+      >
+        <option value="Dia">Dia</option>
+        <option value="Semana">Semana</option>
+        <option
+          value="Hora"
+          disabled={
+            dayjs(startDate).isAfter(changeDayFromToday(1)) ||
+            dayjs(startDate).isBefore(changeDayFromToday(-5))
+          }
+        >
+          Hora em hora
+        </option>
+      </select>
+    </form>
+  );
+};
+
+const OutcomeDay = ({ data, locale }) => {
+  const dayDate = new Date(data?.dt * 1000).toLocaleDateString();
+  return (
+    <div>
+      <p>{locale + " " + dayDate}</p>
+      <p>
+        Temperatura: {data?.temp}
+        °C /{celsiusToFahrenheit(data?.temp)}
+        °F
+      </p>
+      <p>
+        Umidade:
+        {data?.humidity} %
+      </p>
+      <p>
+        Velocidade do vento:
+        {data?.wind_speed} m/s
+      </p>
+    </div>
+  );
+};
+
+const OutcomeWeek = ({ data, locale }) => {
+  return (
+    <div>
+      <div>{locale}</div>
+      {data?.weekly?.map((week, index) => (
+        <div key={index}>
+          <div>{new Date(week.dt * 1000).toLocaleDateString()}</div>
+          <div>
+            Temperatura:{week.temp}°C/{celsiusToFahrenheit(week.temp)}°F
+          </div>
+          <div>Umidade:{week.humidity}</div>
+          <div>Velocidade do vento:{week.wind_speed}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const OutcomeHour = ({ data, locale }) => {
+  return (
+    <div>
+      <div>{locale}</div>
+      {data?.hourly?.map((hour, index) => (
+        <div key={index}>
+          <div>
+            {new Date(hour.dt * 1000).toLocaleDateString() +
+              " " +
+              new Date(hour.dt * 1000).getHours()}
+            :00
+          </div>
+          <div>
+            Temperatura:{hour.temp}°C/{celsiusToFahrenheit(hour.temp)}°F
+          </div>
+          <div>Umidade:{hour.humidity}</div>
+          <div>Velocidade do vento:{hour.wind_speed}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const WeatherApp = () => {
+  const [datePicked, setDatePicked] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [locale, setLocale] = useState("Uberlândia,BR");
+  const [tempViewMode, setTempViewMode] = useState("Dia");
+  const [viewMode, setViewMode] = useState("Dia");
+  const [hourlyData, setHourlyData] = useState(null);
+  const [dailyData, setDailyData] = useState(null);
+  const [weeklyData, setWeeklyData] = useState(null);
+
+  const handleGetWeatherData = async (date) => {
+    const { latitude, longitude } = await getGeocodingWeatherData(locale);
+
+    if (viewMode === "Dia") {
+      const data = await getDailyWeather(date, latitude, longitude);
+      setDailyData(data);
+    } else if (viewMode === "Hora") {
+      const data = await getHourlyWeatherByDay(date, latitude, longitude);
+      setHourlyData(data);
+    } else if (viewMode === "Semana") {
+      const data = await getWeeklyWeatherByDay(date, latitude, longitude);
+      setWeeklyData(data);
+    }
+    setDatePicked(true);
+    setViewMode(tempViewMode);
+  };
+
+  return (
+    <div>
+      <Filter
+        locale={locale}
+        setLocale={setLocale}
+        tempViewMode={tempViewMode}
+        setTempViewMode={setTempViewMode}
+        startDate={startDate}
+      />
+      <Calendar
+        startDate={startDate}
+        setStartDate={setStartDate}
+        tempViewMode={tempViewMode}
+        handleGetWeatherData={handleGetWeatherData}
+      />
+      {datePicked ? (
+        viewMode === "Dia" ? (
+          <OutcomeDay data={dailyData} locale={locale} />
+        ) : viewMode === "Semana" ? (
+          <OutcomeWeek data={weeklyData} locale={locale} />
+        ) : (
+          <OutcomeHour data={hourlyData} locale={locale} />
+        )
+      ) : null}
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WeatherApp />
       </header>
     </div>
   );
---
+++ src/api/index.js
@@ -0,0 +1,146 @@
+import axios from "axios";
+import dayjs from "dayjs";
+
+export async function getGeocodingWeatherData(locale) {
+  return axios
+    .get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${locale}&limit=1&appid=180941f68139fba12f166dc35d9b688b`
+    )
+    .then((response) => {
+      const { lat, lon } = response.data[0];
+      return { latitude: lat, longitude: lon };
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getHistoricalData(latitude, longitude, time) {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${latitude}&lon=${longitude}&dt=${time}&units=metric&appid=180941f68139fba12f166dc35d9b688b`
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getForecastData(latitude, longitude) {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,alerts&units=metric&appid=180941f68139fba12f166dc35d9b688b`
+    )
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export async function getDailyWeather(date, latitude, longitude) {
+  if (date.getTime() + 86400000 < new Date().getTime()) {
+    const unixTimeDate = date.getTime() / 1000;
+
+    const pastData = await getHistoricalData(latitude, longitude, unixTimeDate);
+
+    return pastData.current;
+  } else {
+    const forecastData = await getForecastData(latitude, longitude);
+    const dayIndex = dayjs(date).diff(dayjs(), "day");
+    const dailyData = forecastData.daily[dayIndex];
+    dailyData.temp = dailyData.temp.day;
+    return dailyData;
+  }
+}
+
+export async function getHourlyWeatherByDay(date, latitude, longitude) {
+  if (date.getTime() + 86400000 < new Date().getTime()) {
+    const unixTimeDate = date.getTime() / 1000;
+
+    const pastData = await getHistoricalData(latitude, longitude, unixTimeDate);
+
+    return pastData;
+  } else {
+    const forecastData = await getForecastData(latitude, longitude);
+
+    const isToday = date.getDate() - new Date().getDate() === 0;
+    if (isToday) {
+      forecastData.hourly = forecastData.hourly.slice(0, 24);
+    } else {
+      forecastData.hourly = forecastData.hourly.slice(24, 48);
+    }
+    return forecastData;
+  }
+}
+
+function isAvailable(date) {
+  if (
+    dayjs(date) >= dayjs().subtract(5, "day") &&
+    dayjs(date) <= dayjs().add(7, "day")
+  ) {
+    return true;
+  }
+  return false;
+}
+
+export async function getWeeklyWeatherByDay(date, latitude, longitude) {
+  const daysUntilSundayFromDate = date.getDay();
+  const weeklyData = { weekly: [] };
+  const sundayDate = dayjs(date).subtract(daysUntilSundayFromDate, "day").$d;
+  const forecastData = await getForecastData(latitude, longitude);
+
+  if (sundayDate.getTime() + 86400000 < new Date().getTime()) {
+    const daysUntilSundayFromToday = new Date().getDay();
+    var index = 0;
+    for (var i = daysUntilSundayFromToday; i > 0; i--) {
+      var iDay = dayjs(new Date()).subtract(i, "day").$d;
+      if (isAvailable(iDay)) {
+        const unixTimeDate = Math.round(iDay.getTime() / 1000);
+        const iData = await getHistoricalData(
+          latitude,
+          longitude,
+          unixTimeDate
+        );
+        weeklyData.weekly[index] = iData.current;
+      } else {
+        debugger;
+        weeklyData.weekly[index] = {
+          temp: "indisponível",
+          humidity: "indisponível",
+          wind_speed: "indisponível",
+          dt: iDay.getTime() / 1000,
+        };
+      }
+      index++;
+    }
+    const daysRemaining = 7 - daysUntilSundayFromToday;
+    for (var j = 0; j < daysRemaining; j++) {
+      weeklyData.weekly[index] = forecastData.daily[j];
+      weeklyData.weekly[index].temp = weeklyData.weekly[index].temp.day;
+      index++;
+    }
+    return weeklyData;
+  } else {
+    const sundayIndex = Math.ceil(dayjs(sundayDate).diff(dayjs(), "day", true));
+    for (var k = sundayIndex; k < sundayIndex + 7; k++) {
+      var dayK = dayjs().add(k, "day").$d;
+      if (isAvailable(dayK)) {
+        weeklyData.weekly[k] = forecastData.daily[k];
+        weeklyData.weekly[k].temp = weeklyData.weekly[k].temp.day;
+      } else {
+        debugger;
+        weeklyData.weekly[k] = {
+          temp: "indisponível",
+          humidity: "indisponível",
+          wind_speed: "indisponível",
+          dt: dayK.getTime() / 1000,
+        };
+      }
+    }
+    return weeklyData;
+  }
+}
